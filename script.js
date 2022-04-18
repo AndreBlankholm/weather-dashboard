d8189be6829c9e3f9269f9e57b53163da5f9b663
AndreBlankholm <ablankholm@gmail.com>
2022-04-18 18:13:27 -0500

    setting some fuctionality

--- script.js
+++ script.js
@@ -1,33 +1,69 @@
 
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#search-for-city");
-
+var rootUrl = "https://api.openweathermap.org";
 var weatherApiKey = "05de828319fc1cf4cf97ed816ea2b0fc";
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
 
 var repoContainerEl = document.querySelector("#repos-container");  // <div> element that we'll write all of the repository data to.
 
+var getForecastData = function(lat, lon) {
+  var forecastUrl = `${rootUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
 
-
-var getUserRepos = function (user) {
-    // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=44.9778&lon=93.2650&appid=05de828319fc1cf4cf97ed816ea2b0fc";
-  
-    // make a request to the url
-    fetch(apiUrl).then(function (response) {
-      response.json().then(function (data) {
-        //displayRepos(data, user);
+  fetch(forecastUrl)
+    .then(function(response) {
+      response.json()
+      .then(function(data) {
         console.log(data);
       });
+    });
+};
+
+var updateSearchHistory = function(searchData) {  
+  var newSearchHistoryItem = {
+    city: searchData.name,
+    lat: searchData.lat,
+    lon: searchData.lon
+  };
+
+  if(searchHistory === null) {
+    searchHistory = [];
+  };
+
+  searchHistory.push(newSearchHistoryItem);
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+};
+
+var getCoordinatesByCityName = function (cityName) {
+    // format the github api url
+    var limit = 5;
+    var lat = null;
+    var lon = null;
+    var geoUrl = `${rootUrl}/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${weatherApiKey}`;
+    
+  
+    // make a request to the url
+    fetch(geoUrl).then(function (response) {
+      response.json()
+      .then(function (data) {
+        //displayRepos(data, user);
+        console.log(data[0]);
+        lat = data[0].lat;
+        lon = data[0].lon;
+        getForecastData(lat, lon);
+        updateSearchHistory(data[0]);
+      });
+
     });                                                //-- Now that we've created the function, let's set it up so that when the response data is 
-  };                                                   //   converted to JSON, it will be sent from getUserRepos() to displayRepos() --)
+  };                                                   //   converted to JSON, it will be sent from getCoordinatesByCityName() to displayRepos() --)
   
   var formSubmitHandler = function (event) {
     event.preventDefault();
     // get value from input element
-    var username = nameInputEl.value.trim();           //git the input data from the form and trim all the extra space out of the front and back
+    var cityName = nameInputEl.value.trim();           //git the input data from the form and trim all the extra space out of the front and back
   
-    if (username) {             
-      getUserRepos(username);                           // If there is in fact a value to username, we pass that data to getUserRepos() as an argument
+    if (cityName) {             
+      getCoordinatesByCityName(cityName);                           // If there is in fact a value to cityName, we pass that data to getCoordinatesByCityName() as an argument
       nameInputEl.value = "";                           // then we clear out input feild
     } else {
       alert("Please enter a city");                     // pop up alert for empty input field
@@ -39,11 +75,8 @@ var getUserRepos = function (user) {
   var displayRepos = function(repos, ) {
     console.log(repos);
     
-
     repoContainerEl.textContent = "";                     // be sure to clear out the old content before displaying new content.
     
-
-
     for (var i = 0; i < repos.length; i++) {              // the for loop, we're taking each repository (repos[i]) and writing some of its data to the page
         // loop over repos name
     
